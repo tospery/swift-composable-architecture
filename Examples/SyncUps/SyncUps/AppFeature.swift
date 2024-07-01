b8c27026a3f6dd70be7d0c2676f907254ee9b9cb
tospery <tospery@gmail.com>
2024-07-01 12:02:29 +0800

    format

--- Examples/SyncUps/SyncUps/AppFeature.swift
+++ Examples/SyncUps/SyncUps/AppFeature.swift
@@ -3,81 +3,81 @@ import SwiftUI
 
 @Reducer
 struct AppFeature {
-  @Reducer(state: .equatable)
-  enum Path {
-    case detail(SyncUpDetail)
-    case meeting(Meeting, syncUp: SyncUp)
-    case record(RecordMeeting)
-  }
-
-  @ObservableState
-  struct State: Equatable {
-    var path = StackState<Path.State>()
-    var syncUpsList = SyncUpsList.State()
-  }
-
-  enum Action {
-    case path(StackActionOf<Path>)
-    case syncUpsList(SyncUpsList.Action)
-  }
-
-  @Dependency(\.date.now) var now
-  @Dependency(\.uuid) var uuid
-
-  var body: some ReducerOf<Self> {
-    Scope(state: \.syncUpsList, action: \.syncUpsList) {
-      SyncUpsList()
+    @Reducer(state: .equatable)
+    enum Path {
+        case detail(SyncUpDetail)
+        case meeting(Meeting, syncUp: SyncUp)
+        case record(RecordMeeting)
     }
-    Reduce { state, action in
-      switch action {
-      case let .path(.element(_, .detail(.delegate(delegateAction)))):
-        switch delegateAction {
-        case let .startMeeting(sharedSyncUp):
-          state.path.append(.record(RecordMeeting.State(syncUp: sharedSyncUp)))
-          return .none
+    
+    @ObservableState
+    struct State: Equatable {
+        var path = StackState<Path.State>()
+        var syncUpsList = SyncUpsList.State()
+    }
+    
+    enum Action {
+        case path(StackActionOf<Path>)
+        case syncUpsList(SyncUpsList.Action)
+    }
+    
+    @Dependency(\.date.now) var now
+    @Dependency(\.uuid) var uuid
+    
+    var body: some ReducerOf<Self> {
+        Scope(state: \.syncUpsList, action: \.syncUpsList) {
+            SyncUpsList()
         }
-
-      case .path:
-        return .none
-
-      case .syncUpsList:
-        return .none
-      }
+        Reduce { state, action in
+            switch action {
+            case let .path(.element(_, .detail(.delegate(delegateAction)))):
+                switch delegateAction {
+                case let .startMeeting(sharedSyncUp):
+                    state.path.append(.record(RecordMeeting.State(syncUp: sharedSyncUp)))
+                    return .none
+                }
+                
+            case .path:
+                return .none
+                
+            case .syncUpsList:
+                return .none
+            }
+        }
+        .forEach(\.path, action: \.path)
     }
-    .forEach(\.path, action: \.path)
-  }
 }
 
 struct AppView: View {
-  @Bindable var store: StoreOf<AppFeature>
-
-  var body: some View {
-    NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-      SyncUpsListView(
-        store: store.scope(state: \.syncUpsList, action: \.syncUpsList)
-      )
-    } destination: { store in
-      switch store.case {
-      case let .detail(store):
-        SyncUpDetailView(store: store)
-      case let .meeting(meeting, syncUp):
-        MeetingView(meeting: meeting, syncUp: syncUp)
-      case let .record(store):
-        RecordMeetingView(store: store)
-      }
+    @Bindable var store: StoreOf<AppFeature>
+    
+    var body: some View {
+        NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+            SyncUpsListView(
+                store: store.scope(state: \.syncUpsList, action: \.syncUpsList)
+            )
+        } destination: { store in
+            switch store.case {
+            case let .detail(store):
+                SyncUpDetailView(store: store)
+            case let .meeting(meeting, syncUp):
+                MeetingView(meeting: meeting, syncUp: syncUp)
+            case let .record(store):
+                RecordMeetingView(store: store)
+            }
+        }
     }
-  }
 }
 
 #Preview {
-  @Shared(.syncUps) var syncUps = [
-    .mock,
-    .productMock,
-    .engineeringMock,
-  ]
-  return AppView(
-    store: Store(initialState: AppFeature.State()) {
-      AppFeature()
-    }
-  )
+    @Shared(.syncUps) var syncUps = [
+        .mock,
+        .productMock,
+        .engineeringMock,
+    ]
+    return AppView(
+        store: Store(initialState: AppFeature.State()) {
+            AppFeature()
+        }
+    )
 }
